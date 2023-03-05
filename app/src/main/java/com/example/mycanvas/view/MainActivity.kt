2c20a6ad292430183321c5769d66d3cda0a8a245
mj.alijani <mj.alijani@gmail.com>
2023-03-05 16:57:58 +0330

    colorize routes & find close points

--- app/src/main/java/com/example/mycanvas/view/MainActivity.kt
+++ app/src/main/java/com/example/mycanvas/view/MainActivity.kt
@@ -1,16 +1,25 @@
 package com.example.mycanvas.view
 
+import android.location.Location
+import android.os.Build
 import android.os.Bundle
 import android.preference.PreferenceManager
+import android.util.Log
+import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import com.example.mycanvas.R
 import com.example.mycanvas.model.GeoData
+import com.example.mycanvas.model.PointWithCoordinates
+import com.example.mycanvas.model.PointsWithDistance
+import com.google.android.gms.maps.model.LatLng
 import com.google.gson.Gson
 import org.osmdroid.config.Configuration.getInstance
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory
 import org.osmdroid.util.GeoPoint
 import org.osmdroid.views.MapView
+import org.osmdroid.views.overlay.Marker
 import org.osmdroid.views.overlay.Polygon
 import org.osmdroid.views.overlay.Polyline
 import java.io.IOException
@@ -18,10 +27,16 @@ import java.io.InputStream
 
 
 class MainActivity : AppCompatActivity() {
+
     private val REQUEST_PERMISSIONS_REQUEST_CODE = 1
     private lateinit var map: MapView
     private val polyline = ArrayList<List<Any>>()
     private val polygon = ArrayList<List<Any>>()
+    private val points = ArrayList<PointWithCoordinates>()
+    private val pointsWithDistance = ArrayList<PointsWithDistance>()
+    private val closePoints = ArrayList<PointsWithDistance>()
+
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -34,17 +49,13 @@ class MainActivity : AppCompatActivity() {
         //note, the load method also sets the HTTP User Agent to your application's package name, if you abuse osm's
         //tile servers will get you banned based on this string.
 
-        //inflate and create the map
-        setContentView(R.layout.activity_main)
-
-
         map = findViewById(R.id.map)
         map.setTileSource(TileSourceFactory.MAPNIK)
 
         val mapController = map.controller
         mapController.setZoom(15.5)
-        val startPoint = GeoPoint(35.7844311386761, 51.360950675604784)
-        mapController.setCenter(startPoint)
+//        val startPoint = GeoPoint(35.7844311386761, 51.360950675604784)
+//        mapController.setCenter(startPoint)
         val geoData = loadJSONFile()
 
         // fill the polygon & polyline
@@ -55,8 +66,8 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
-
-//add your points here
+        //<editor-fold desc="** Polygon **">
+        //add your points here
         val osmPolygon = Polygon();    //see note below
         val geoPoints = ArrayList<GeoPoint>()
         for (poly in polygon[0]) {
@@ -81,11 +92,20 @@ class MainActivity : AppCompatActivity() {
         osmPolygon1.fillPaint.color = getColor(R.color.white_transparent) //set fill color
         osmPolygon1.points = geoPoints1
         map.overlays.add(osmPolygon1)
-
+        //</editor-fold>
 
         val line = ArrayList<Polyline>()  //see note below
-
         val lineGeo = ArrayList<GeoPoint>()
+        val colors = arrayListOf(
+            R.color.black,
+            R.color.purple_200,
+            R.color.teal_200,
+            R.color.white,
+            R.color.purple_500,
+            R.color.purple_700,
+            R.color.purple_800,
+            R.color.purple_900,
+        )
 
         for (i in polyline.indices) {
             line.add(Polyline())
@@ -93,10 +113,104 @@ class MainActivity : AppCompatActivity() {
 
             for (polyLines in polyline[i]) {
                 val geos: List<Double> = polyLines as List<Double>
+                points.add(PointWithCoordinates(i, polyline[i], geos[1], geos[0]))
                 lineGeo.add(GeoPoint(geos[1], geos[0]))
                 line[i].setPoints(lineGeo)
+                line[i].color = getColor(colors[i])
+                line[i].setOnClickListener { _, _, _ ->
+                    Toast.makeText(applicationContext, "$i", Toast.LENGTH_SHORT).show()
+                    true
+                }
             }
             map.overlays.add(line[i])
+        }
+
+        points.forEach {
+            distanceInMeter(it, points)
+        }
+
+        closePoints.forEach {
+            val startMarker = Marker(map)
+            startMarker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM)
+            val startPoint = GeoPoint(it.sLatLng.latitude, it.sLatLng.longitude)
+            startMarker.position = startPoint;
+            map.overlays.add(startMarker)
+
+            startMarker.setOnMarkerClickListener { marker, mapView ->
+                Toast.makeText(applicationContext, "coordinatesId : ${it.coordinatesId}", Toast.LENGTH_SHORT).show()
+                true
+            }
+
+            val sstartMarker = Marker(map)
+            sstartMarker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM)
+            val sstartPoint = GeoPoint(it.tLatLng.latitude, it.tLatLng.longitude)
+            sstartMarker.position = sstartPoint;
+            map.overlays.add(sstartMarker)
+        }
+
+        mapController.setCenter(
+            GeoPoint(
+                closePoints[0].sLatLng.latitude,
+                closePoints[0].sLatLng.longitude
+            )
+        )
+        closePoints
+    }
+
+    private fun distanceInMeter(
+        singleLatLng: PointWithCoordinates,
+        locations: ArrayList<PointWithCoordinates>,
+    ) {
+        val results = FloatArray(1)
+
+        locations.forEach lit@{ location ->
+
+//            if (singleLatLng.coordinatesId == location.coordinatesId) {
+//                return@lit
+//            }
+
+            if (location.coordinates.any { it == singleLatLng.coordinates }) {
+                return@lit
+            }
+
+            Log.d(
+                "TAG",
+                "distanceInMeter: ${singleLatLng.coordinatesId} *** ${location.coordinatesId} "
+            )
+
+            if (location.coordinates != singleLatLng.coordinates) {
+
+                Location.distanceBetween(
+                    singleLatLng.lat,
+                    singleLatLng.lng,
+                    location.lat,
+                    location.lng,
+                    results
+                )
+                if (results[0] != 0f) {
+                    pointsWithDistance.add(
+                        PointsWithDistance(
+                            singleLatLng.coordinatesId,
+                            results[0],
+                            singleLatLng.coordinates,
+                            LatLng(singleLatLng.lat, singleLatLng.lng),
+                            LatLng(location.lat, location.lng),
+
+                            )
+                    )
+                    if (results[0] <= 40f) {
+                        closePoints.add(
+                            PointsWithDistance(
+                                singleLatLng.coordinatesId,
+                                results[0],
+                                singleLatLng.coordinates,
+                                LatLng(singleLatLng.lat, singleLatLng.lng),
+                                LatLng(location.lat, location.lng),
+                            )
+                        )
+                    }
+                }
+            }
         }
     }
 
